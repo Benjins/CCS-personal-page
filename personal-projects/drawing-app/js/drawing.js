3c7c52c572b102484bfd0fe804707564ed628ad1
Benji Smith <smith.benj@husky.neu.edu>
2015-02-26 18:07:41 -0500

    Drawing App: icons, selection basics, etc.

--- personal-projects/drawing-app/js/drawing.js
+++ personal-projects/drawing-app/js/drawing.js
@@ -1,21 +1,18 @@
-/*
-Circles
-Rectangles
-
-methods:
-updateSVG()
-onDragStart()
-onDrag()
-onDragEnd()
-
-*/
-
 var svgElements = [];
-
-
 var svgContainerOffset;
+var shapeType = "Rect"
+var shapeFill = "#ff0000";
+
+function changeShapeType(newType){
+	shapeType = newType;
+}
+
 window.onload = function(){
 	svgContainerOffset = document.getElementById("svg-container").getBoundingClientRect();
+}
+
+function pickColor(){
+	
 }
 
 var SVGCirc = function(x, y, r, col){
@@ -26,8 +23,15 @@ var SVGCirc = function(x, y, r, col){
 	this.id = "svgElem_" + svgElements.length;
 }
 
+function selectElement(elem){
+	var bounds = document.getElementById(elem.id).getBoundingClientRect();
+	console.log(bounds);
+}
+
 SVGCirc.prototype.Add = function(){
 	var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+	circle.ondragstart = function() { return false; };
+	circle.onclick = function(){selectElement(circle);}
 	circle.setAttributeNS(null, "cx",this.x);
 	circle.setAttributeNS(null, "cy",this.y);
 	circle.setAttributeNS(null, "r", this.r);
@@ -59,6 +63,8 @@ var SVGRect = function(x, y, w, h, col){
 
 SVGRect.prototype.Add = function(){
 	var rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+	rect.ondragstart = function() { return false; };
+	rect.onclick = function(){selectElement(rect);}
 	rect.setAttributeNS(null, "x",this.x);
 	rect.setAttributeNS(null, "y",this.y);
 	rect.setAttributeNS(null, "width",this.w);
@@ -81,20 +87,14 @@ SVGRect.prototype.Update = function(x, y){
 	rect.setAttribute("height", this.h);
 }
 
-//Remove
-//Add
-//Update
-
-var type = "Rect"
-
 function AddElem(x, y){
-	if(type == "Circ"){
-		var newCirc = new SVGCirc(x, y, 2, "red");
+	if(shapeType == "Circ"){
+		var newCirc = new SVGCirc(x, y, 0, "red");
 		svgElements.push(newCirc);
 		newCirc.Add();
 	}
-	else if(type == "Rect"){
-		var newRect = new SVGRect(x, y, 2, 2, "red");
+	else if(shapeType == "Rect"){
+		var newRect = new SVGRect(x, y, 0, 0, "red");
 		svgElements.push(newRect);
 		newRect.Add();
 	}
@@ -134,7 +134,16 @@ document.onkeyup     = function(event) {
 								} 
 							};
 document.onmousedown = function(e) { if(e.which == 1){isMouseDown = true; AddElem(TrueX(e), TrueY(e));}};
-document.onmouseup   = function(e) { if(e.which == 1){isMouseDown = false; }};
+document.onmouseup   = function(e) { 
+							if(e.which == 1){
+								isMouseDown = false;
+								if(svgElements.length > 0 && Math.pow(TrueX(e) - svgElements[svgElements.length-1].x, 2)
+														   + Math.pow(TrueY(e) - svgElements[svgElements.length-1].y, 2) < 2){
+									svgElements[svgElements.length-1].Remove();
+									svgElements.pop();
+								}
+							}
+						};
 document.onmousemove = function(e) { if(e.which == 1){if(isMouseDown && svgElements.length > 0) {svgElements[svgElements.length-1].Update(TrueX(e), TrueY(e));} }};
 
 
