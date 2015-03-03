a44cea8c7df8d37f17ccccc201f41010b36f80c4
Benji Smith <smith.benj@husky.neu.edu>
2015-03-02 23:37:38 -0500

    Initialised harp.

---
+++ _harp/personal-projects/drawing-app/js/drawing.js
@@ -0,0 +1,162 @@
+var svgElements = [];
+var svgContainerOffset;
+var shapeType = "Rect"
+var shapeFill = "#ff0000";
+
+function changeShapeType(newType){
+	shapeType = newType;
+}
+
+window.onload = function(){
+	svgContainerOffset = document.getElementById("svg-container").getBoundingClientRect();
+}
+
+function pickColor(){
+	var elem = document.getElementById("color-picker");
+	if(elem.style.display === ""){
+		elem.style.display = "none";
+	}
+	else{
+		elem.style.display = "";
+	}
+}
+
+var SVGCirc = function(x, y, r, col){
+	this.x = x;
+	this.y = y;
+	this.startX = x;
+	this.startY = y;
+	this.r = r;
+	this.color = col;
+	this.id = "svgElem_" + svgElements.length;
+}
+
+function selectElement(elem){
+	var bounds = document.getElementById(elem.id).getBoundingClientRect();
+	console.log(bounds);
+}
+
+SVGCirc.prototype.Add = function(){
+	var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+	circle.ondragstart = function() { return false; };
+	circle.onclick = function(){selectElement(circle);}
+	circle.setAttributeNS(null, "cx",this.x);
+	circle.setAttributeNS(null, "cy",this.y);
+	circle.setAttributeNS(null, "r", this.r);
+	circle.setAttributeNS(null, "fill", "#ff0000");
+	circle.setAttributeNS(null, "id", this.id);
+	document.getElementById("svg-container").appendChild(circle);
+}
+
+SVGCirc.prototype.Remove = function(){
+	var elem = document.getElementById(this.id);
+	elem.parentElement.removeChild(elem);
+}
+
+SVGCirc.prototype.Update = function(x, y){
+	this.r = Math.max(Math.abs(x - this.x), Math.abs(y - this.y));
+	var circle = document.getElementById(this.id);
+	circle.setAttribute("r", this.r);
+}
+
+var SVGRect = function(x, y, w, h, col){
+	this.startX = x;
+	this.startY = y;
+	this.x = x;
+	this.y = y;
+	this.w = w;
+	this.h = h;
+	this.color = col;
+	this.id = "svgElem_" + svgElements.length;
+}
+
+SVGRect.prototype.Add = function(){
+	var rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+	rect.ondragstart = function() { return false; };
+	rect.onclick = function(){selectElement(rect);}
+	rect.setAttributeNS(null, "x",this.x);
+	rect.setAttributeNS(null, "y",this.y);
+	rect.setAttributeNS(null, "width",this.w);
+	rect.setAttributeNS(null, "height",this.h);
+	rect.setAttributeNS(null, "fill", "#00ff00");
+	rect.setAttributeNS(null, "id", this.id);
+	document.getElementById("svg-container").appendChild(rect);
+}
+
+SVGRect.prototype.Remove = function(){
+	var elem = document.getElementById(this.id);
+	elem.parentElement.removeChild(elem);
+}
+
+SVGRect.prototype.Update = function(x, y){
+	this.x = Math.min(x, this.startX);
+	this.y = Math.min(y, this.startY);
+	this.w = Math.abs(this.startX - x);
+	this.h = Math.abs(this.startY - y);
+	var rect = document.getElementById(this.id);
+	rect.setAttribute("width",  this.w);
+	rect.setAttribute("height", this.h);
+	rect.setAttribute("x", this.x);
+	rect.setAttribute("y", this.y);
+}
+
+function AddElem(x, y){
+	if(shapeType == "Circ"){
+		var newCirc = new SVGCirc(x, y, 0, "red");
+		svgElements.push(newCirc);
+		newCirc.Add();
+	}
+	else if(shapeType == "Rect"){
+		var newRect = new SVGRect(x, y, 0, 0, "red");
+		svgElements.push(newRect);
+		newRect.Add();
+	}
+}
+
+function TrueX(e){
+	var x;
+	if (e.pageX && e.pageX !== NaN) { 
+	  x = e.pageX;
+	}
+	else { 
+	  x = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+	} 
+	x -= svgContainerOffset.left;
+	
+	return x;
+}
+
+function TrueY(e){
+	var y;
+	if (e.pageY && e.pageY !== NaN) { 
+	  y = e.pageY;
+	}
+	else { 
+	  y = e.clientY + document.body.scrollTop + document.documentElement.scrollTop; 
+	} 
+	y -= svgContainerOffset.top;
+	
+	return y;
+}
+
+var isMouseDown = false;
+document.onkeyup     = function(event) { 
+								var evtobj = event? event : e; 
+								if (evtobj.keyCode == 90 && evtobj.ctrlKey && svgElements.length > 0){
+									svgElements.pop().Remove();
+								} 
+							};
+document.onmousedown = function(e) { if(e.which == 1){isMouseDown = true; AddElem(TrueX(e), TrueY(e));}};
+document.onmouseup   = function(e) { 
+							if(e.which == 1){
+								isMouseDown = false;
+								if(svgElements.length > 0 && Math.pow(TrueX(e) - svgElements[svgElements.length-1].startX, 2)
+														   + Math.pow(TrueY(e) - svgElements[svgElements.length-1].startY, 2) < 2){
+									svgElements[svgElements.length-1].Remove();
+									svgElements.pop();
+								}
+							}
+						};
+document.onmousemove = function(e) { if(e.which == 1){if(isMouseDown && svgElements.length > 0) {svgElements[svgElements.length-1].Update(TrueX(e), TrueY(e));} }};
+
+
