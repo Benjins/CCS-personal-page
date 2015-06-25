bc87bca307d3630d49e2b3337c66449ac18eec96
Benji Smith <smith.benj@husky.neu.edu>
2015-06-24 20:49:00 -0400

    Testing cpp-check js stuff.

---
+++ personal-projects/my-engine/js/check-parse.js
@@ -0,0 +1,83 @@
+//Expects --template '{file},{line},{severity},{id},{message}'
+
+var Error = function(file, line, id, severity, message){
+	this.file = file;
+	this.line = line;
+	this.id = id;
+	this.severity = severity;
+	this.message = message;
+}
+
+
+function GetAllErrorsFromFile(fileName){
+	var request = new XMLHttpRequest();
+	request.open("GET", fileName);
+	request.onreadystatechange = function(){
+		if (request.readyState===4 && request.status===200){
+			var errors = GetErrorsFromContents(request.responseText);
+
+			var container = document.getElementById("error-container");
+			for(var idx in errors){
+				var error = errors[idx];
+				AddErrorDisplayToContainer(container, error);
+			}
+		}
+		else if(request.readyState===4){
+			console.log("GetAllErrorsFromFile: Error, server responded with error code: '" + request.status + "'.");
+		}
+	};
+}
+
+function GetErrorsFromContents(fileContents){
+	var lines = fileContents.split("\n");
+
+	var errors = [];
+
+	for(var idx in lines){
+		var line = lines[idx];
+		if(line.trim){ //Some older browsers may not support this
+			line = line.trim();
+		}
+		else{
+			line = line.replace(/^\s+|\s+$/gm,'');
+		}
+
+		if(line.length > 0){
+			errors.push(ParseError(line));
+		}		
+	}
+
+	return errors;
+}
+
+function ParseError(line){
+	var lineSplit = line.split(",");
+
+	var file = lineSplit[0];
+	var line = parseInt(lineSplit[1]);
+	var id = lineSplit[2];
+	var severity = lineSplit[3];
+	var message = lineSplit[4];
+
+	return new Error(file, line, id, severity, message);
+}
+
+
+function AddErrorDisplayToContainer(container, error){
+	var errorDiv = document.createElement('div');
+	errorDiv.class = "error";
+
+	var fileInfo = document.createElement('p');
+	var lineInfo = document.createElement('p');
+	var messageInfo = document.createElement('p');
+
+	fileInfo.innerHTML = error.file;
+	lineInfo.innerHTML = error.line;
+	messageInfo.innerHTML = error.message;
+
+	errorDiv.appendChild(fileInfo);
+	errorDiv.appendChild(lineInfo);
+	errorDiv.appendChild(messageInfo);
+
+	container.appendChild(errorDiv);
+}
