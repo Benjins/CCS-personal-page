6b663173c49f5383c7d0016b05be7a7a6fd15684
Benji Smith (Lab) <smith.benj@husky.neu.edu>
2015-09-23 16:20:07 -0400

    Basic comments.  Client only.

---
+++ personal-projects/comments/js/client.js
@@ -0,0 +1,43 @@
+function sendUserComment(user, comment){
+	var commentObj = {};
+	commentObj['user'] = user;
+	commentObj['comment'] = comment;
+
+	var req = new XMLHttpRequest();
+	req.open("POST", "http://45.55.247.145/comments/upload.php");
+	req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+	req.send(JSON.stringify(commentObj));
+	console.log(JSON.stringify(commentObj));
+}
+
+function sendComment(){
+	var nameField = document.getElementById('nameField');
+	var commentField = document.getElementById('commentField');
+
+	sendUserComment(nameField.value, commentField.value);
+}
+
+function getComments(){
+	var req = new XMLHttpRequest();
+	req.open("GET", "http://45.55.247.145/comments/upload.php", true);
+	req.onreadystatechange = function() {
+	    if (req.readyState == 4) {
+		showComments(JSON.parse(req.responseText));
+	    }
+	};
+
+	req.send();
+}
+
+function showComments(commentList){
+	var commentListElem = document.getElementById('comments-list');
+
+	for(var idx in commentList){
+		var cmmt = commentList[idx];
+		var cmmtNode = document.createElement('li');
+		cmmtNode.innerHTML = cmmt.user + ": '" + cmmt.comment + "'";
+		commentListElem.appendChild(cmmtNode);
+	}
+}
+
+
